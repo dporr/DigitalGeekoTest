290a48e9e9660f82822feefd4b21710e0d3742d4
Diego Porras <andree.rivas@galileo.edu>
2017-04-18 15:09:55 -0600

    Actualizacion final de variables globales

--- app/src/main/java/test/dporrras/digitalgeeko/GlobalValues.java
+++ app/src/main/java/test/dporrras/digitalgeeko/GlobalValues.java
@@ -1,8 +1,20 @@
 package test.dporrras.digitalgeeko;
 
-/**
- * Created by vectordxt77 on 18/04/17.
- */
-
+/*@author dporras
+*Set of constants useful across the program
+*/
 public interface GlobalValues {
+    public static final String DATASET = "DATASET";
+    public static final String MIN_COL = "MIN_COL";
+    public static final String MAX_COL = "MAX_COL";
+    public static final String ROW_NAME = "ROW_NAME";
+    public static final String FOOTBALL_DS = "footbal.dat";
+    public static final String WHEATER_DS = "wheater.dat";
+    public static final int F_MAX_COL = 7;
+    public static final int F_MIN_COL = 9;
+    public static int F_ROW_NAME = 2;
+    public static final int W_MAX_COL = 2;
+    public static final int W_MIN_COL = 3;
+    public static int W_ROW_NAME = 1;
+    ;
 }
