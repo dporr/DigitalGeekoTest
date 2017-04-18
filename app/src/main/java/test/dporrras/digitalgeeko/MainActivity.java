9bb06bca9eba8f12a33e0a290670855c69caa935
Diego Porras <andree.rivas@galileo.edu>
2017-04-17 22:05:35 -0600

    mfirst commit

---
+++ app/src/main/java/test/dporrras/digitalgeeko/MainActivity.java
@@ -0,0 +1,18 @@
+package test.dporrras.digitalgeeko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
