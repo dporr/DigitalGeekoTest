7812ba28d5e63f29fc5a64a99d187e754f33be76
Diego Porras <andree.rivas@galileo.edu>
2017-04-18 12:59:51 -0600

    Iniciando refactoring

---
+++ app/src/main/java/test/dporrras/digitalgeeko/ResultsActivity.java
@@ -0,0 +1,13 @@
+package test.dporrras.digitalgeeko;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
