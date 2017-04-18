368cc15a66cd3aa5a8e5fc2c870a97b93b861422
Diego Porras <andree.rivas@galileo.edu>
2017-04-17 22:07:50 -0600

    Parseo inicial del archivo football.dat

--- app/src/main/java/test/dporrras/digitalgeeko/MainActivity.java
+++ app/src/main/java/test/dporrras/digitalgeeko/MainActivity.java
@@ -15,4 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * @author: dporras
+     * @param filename to open
+     * Open a raw .dat file and parses the text
+     * **/
+    private void parseFile(String filename){
+        //Uso de api 19 por soportortar try-with-resources
+        try(InputStream file = getApplicationContext().getResources().openRawResource(R.raw.football)){
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("\n");
+            while(scanner.hasNext()) Log.i("message: ",scanner.next());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
